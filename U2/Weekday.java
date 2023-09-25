0816f12687cd53b0fe4f7a585e058622e81c7d7b
Linuxe <angelren1220@gmail.com>
2023-09-25 18:15:54 -0400

    switch case'

--- U2/Weekday.java
+++ U2/Weekday.java
@@ -1,30 +1,55 @@
 /*Write a program that will generate and display a random number from 1 to 7 and display the corresponding day of the week where 1 for Monday, 2 for Tuesday and so on.*/
 
-import java.util.Scanner;
-
 class Weekday {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter a number (1 to 7)");
-    int number = scanner.nextInt();
-    scanner.close();
+    // generate a random number bewteen 1 and 7;
+    int number = (int)(Math.random() * 7 + 1);
 
-    if (number == 1) {
-      System.out.println("Monday");
-    } else if (number == 2) {
-      System.out.println("Tuesday");
-    } else if (number == 3) {
-      System.out.println("Wednesday");
-    } else if (number == 4) {
-      System.out.println("Thursday");
-    } else if (number == 5) {
-      System.out.println("Friday");
-    } else if (number == 6) {
-      System.out.println("Saturday");
-    } else if (number == 6){
-      System.out.println("Sunday");
-    } else {
-      System.out.println("Wrong input.");
+    // if statement
+
+    // if (number == 1) {
+    //   System.out.println("Monday");
+    // } else if (number == 2) {
+    //   System.out.println("Tuesday");
+    // } else if (number == 3) {
+    //   System.out.println("Wednesday");
+    // } else if (number == 4) {
+    //   System.out.println("Thursday");
+    // } else if (number == 5) {
+    //   System.out.println("Friday");
+    // } else if (number == 6) {
+    //   System.out.println("Saturday");
+    // } else if (number == 6){
+    //   System.out.println("Sunday");
+    // } else {
+    //   System.out.println("Error.");
+    // }
+
+    // switch case
+    switch(number){
+      case 1:
+        System.out.println("Monday");
+        break;
+      case 2:
+        System.out.println("Tuesday");
+        break;
+      case 3:
+        System.out.println("Wednesday");
+        break;
+      case 4:
+        System.out.println("Thursday");
+        break;
+      case 5:
+        System.out.println("Friday");
+        break;
+      case 6:
+        System.out.println("Saturday");
+        break;
+      case 7:
+        System.out.println("Sunday");
+        break;
+      default:
+        System.out.println("Error");
     }
 
   }
