4ce0c14b8fadd273b59449394666b2fe66368de6
Linuxe <angelren1220@gmail.com>
2023-09-25 17:15:36 -0400

    add codes

---
+++ U2/RandomNumber.java
@@ -0,0 +1,8 @@
+class RandomNumber {
+  public static void main(String[] args){
+    // generate a randome number between 0 and 100
+    int randomNum = (int)(Math.random()*100 + 1);
+    System.out.println("The random number is " + randomNum);
+
+  }
+}
