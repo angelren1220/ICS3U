b8aec8d66ca427f00e1827a21179cd29ae83e542
Linuxe <angelren1220@gmail.com>
2023-09-28 19:10:46 -0400

    methods

--- U3/SumEvenNumbers.java
+++ U3/SumEvenNumbers.java
@@ -1,3 +1,5 @@
+/* Write a static method in a program that will calculate and display the sum of the series 2+4+6+…….+ N. The program must ask user to enter the value of N as an even number.  */
+
 import java.util.Scanner;
 
 class SumEvenNumbers {
@@ -5,15 +7,19 @@ class SumEvenNumbers {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Enter an even number N");
     int n = scanner.nextInt();
-
     scanner.close();
 
+    sumEvenNumbers(n);
+  }
+  
+  public static void sumEvenNumbers(int n){
     int sum = 0;
-
+  
     for(int i = 2; i < n + 1; i+=2 ){
       sum += i;
     }
-
+  
     System.out.println("The sum is " + sum);
+    
   }
 }
