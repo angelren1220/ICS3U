dc96fb6ff289275ffbb9e5f732dc444df8b24d42
Linuxe <angelren1220@gmail.com>
2023-10-02 17:58:05 -0400

    add answers

--- U3/CalculateArea.java
+++ U3/CalculateArea.java
@@ -7,6 +7,7 @@ class CalculateArea {
     System.out.println("Choose a shape:");
     System.out.println("1. Renctangle");
     System.out.println("2. Circle");
+    System.out.println("3. Triangle");
     int option = scanner.nextInt();
 
     switch (option) {
@@ -15,17 +16,27 @@ class CalculateArea {
         System.out.println("Enter width:");
         float width = scanner.nextFloat();
         System.out.println("Enter height:");
-        float height = scanner.nextFloat();
-        float renctangleArea = width * height;
-        System.out.println("Area is " + renctangleArea);
+        float height1 = scanner.nextFloat();
+        float rectangleArea = rectangleArea(height1, width);
+        System.out.println("Area is " + rectangleArea);
         break;
 
       case 2:
         System.out.println("Circle");
         System.out.println("Enter radius:");
         float radius = scanner.nextFloat();
-        float circleArea = (float)(Math.PI * Math.pow(radius, 2));
+        float circleArea = circleArea(radius);
         System.out.println("Area is " + circleArea);
+        break;
+
+      case 3:
+        System.out.println("Triangle");
+        System.out.println("Enter height:");
+        float height2 = scanner.nextFloat();
+        System.out.println("Enter base:");
+        float base = scanner.nextFloat();
+        float triangleArea = triangleArea(height2, base);
+        System.out.println("Area is " + triangleArea);
         break;
 
       default:
@@ -35,4 +46,18 @@ class CalculateArea {
     scanner.close();
 
   }
+
+  public static float rectangleArea(float h, float w){
+    return w * h;
+  }
+
+  public static float circleArea(float r){
+    return (float) (Math.PI * Math.pow(r, 2));
+  }
+
+  public static float triangleArea(float h, float b){
+    return h * b / 2;
+  }
+
+
 }
