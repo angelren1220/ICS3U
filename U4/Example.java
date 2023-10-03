9b83cc85cd80a8281b25c54bb5e37fbfba1e973e
Linuxe <angelren1220@gmail.com>
2023-10-03 17:42:42 -0400

    add codes

---
+++ U4/Example.java
@@ -0,0 +1,14 @@
+class Example {
+  public static void main(String[] args){
+
+    // declare initialize an int array
+    int[] arr = {1, 2, 3};
+
+    // access first element in the array and print it out
+    System.out.println(arr[0]);
+
+    // access second element in the array and modify its value
+    arr[1] = 0;
+    
+  }
+}
