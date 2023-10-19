1d0fc851ac56a0820b427505eb51bd6088674bcf
Linuxe <angelren1220@gmail.com>
2023-10-19 01:08:14 -0400

    add updatItem

--- Project/StoreManagementSystem.java
+++ Project/StoreManagementSystem.java
@@ -16,9 +16,9 @@ public class StoreManagementSystem {
     Scanner scanner = new Scanner(System.in);
 
     // declare contents of store managment system
-    String[] items = {"dry food", "canned food", "litter", "toy", "collar"};
-    int[] stock = {44, 90, 100, 20, 8};
-    double[] price = {69.99, 48.50, 9.99, 4.59, 18.45};
+    String[] items = { "treat", "can", "litter", "toy", "collar" };
+    int[] stock = { 44, 90, 100, 20, 8 };
+    double[] price = { 69.99, 48.50, 9.99, 4.59, 18.45 };
 
     // Menu for users
     System.out.println("====== Ms.Shiro's Store Management System ====== ");
@@ -41,16 +41,16 @@ public class StoreManagementSystem {
 
         case 2: // search item
           int index = searchItem(items, scanner);
-          
-          if(index != -1){
-            System.out.println(stock[index] + " in stock price: $" + price[index]);
+
+          if (index != -1) {
+            System.out.println("Index: " + index + items[index] + stock[index] + " in stock price: $" + price[index]);
           } else {
             System.out.println("Could not find the item.");
           }
           break;
 
         case 3: // update item
-          updateItem();
+          updateItem(items, scanner);
           break;
 
         case 4: // exit
@@ -70,23 +70,30 @@ public class StoreManagementSystem {
   /* Methods */
   public static void viewAllItems(String[] items, int[] stock, double[] price) {
     System.out.println("Name\tStock\tPrice($)");
-    for(int i = 0; i < items.length; i++){
-      System.out.println(items[i]+"\t"+stock[i]+"\t"+price[i]);
+    for (int i = 0; i < items.length; i++) {
+      System.out.println(items[i] + "\t" + stock[i] + "\t" + price[i]);
     }
   }
 
   public static int searchItem(String[] items, Scanner scanner) {
     System.out.println("Enter item name to search:");
     String name = scanner.nextLine();
-    for(int i = 0; i < items.length; i++){
-      if(name.equals(items[i])){
+    for (int i = 0; i < items.length; i++) {
+      if (name.equals(items[i])) {
         return i;
       }
     }
     return -1;
   }
 
-  public static void updateItem() {
-    System.out.println("update item");
+  public static void updateItem(String[] items, Scanner scanner) {
+    System.out.println("Enter the item index to be updated:");
+    int index = scanner.nextInt();
+    scanner.nextLine(); // consume new line
+
+    System.out.println("Enter the name:");
+    String name = scanner.nextLine();
+    items[index] = name;
+
   }
 }
