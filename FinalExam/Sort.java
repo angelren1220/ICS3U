64d1e10acb158525b9aa803468508d7d7ca286db
Linuxe <angelren1220@gmail.com>
2023-10-24 15:00:12 -0400

    add Q7

---
+++ FinalExam/Sort.java
@@ -0,0 +1,24 @@
+/* What is algorithm? Write algorithm using pseudo code to sort a list of numbers. */
+
+// Algorithm is a process or set of rules or steps to solve a problem. Before writing a program, you need to find out the steps to be followed to solve a problem. This process is called algorithm. Algorithm is written in simple English like code called pseudo code which is not specific to any programming language.
+
+// Algorithm: Bubble Sort
+// Input: A list of numbers L
+// Output: A sorted list L'
+
+// 1. Begin
+// 2. Do
+//     2.1 Set 'swapped' to False
+//     2.2 For i from 0 to length of L - 2
+//         2.2.1 If L[i] > L[i+1]
+//             2.2.1.1 Swap L[i] with L[i+1]
+//             2.2.1.2 Set 'swapped' to True
+//     2.3 End For
+// 3. While 'swapped' is True
+// 4. Return L
+// 5. End
+
+
+public class Sort {
+  
+}
